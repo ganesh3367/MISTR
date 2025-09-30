e62c3c581c13a9c0af5362ff8d836506ca77d50d
ganesh3367 <ganeshpwayal@gmail.com>
2025-09-30 23:07:50 +0530

    Initial commit

--- my-app/App.js
+++ my-app/App.js
@@ -1,7 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import Controls from 'my-app/control.js';
+import TimerDisplay from './Timer_display';
+// ...existing code...
 
 export default function App() {
+  return (
+    <View style={styles.container}>
+      <TimerDisplay />
+      <Controls />
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
